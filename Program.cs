d12f053269cbe53122cb81dd970dc8e112c19788
lebuinhatduy <lebuinhatduyyy@gmail.com>
2025-02-10 09:00:34 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("nhập họ và tên");
+        string("")
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("Nhập số thứ nhất:"); // cout
+        double sothunhat = Convert.ToDouble(Console.ReadLine());
+
+        /*
+        double so1;
+        Console.WriteLine("Nhập số thứ nhất:"); // cout
+        so1 = convert.todouble(console.readline()); // chuỗi văn bản 
+        
+
+        Console.WriteLine("nhập số thứ hai:");
+        double sothuhai = Convert.ToDouble(Console.ReadLine());
+
+        double tong = sothunhat + sothuhai;
+
+        Console.WriteLine("tổng của hai số là: " + tong);
+        Console.WriteLine("{0}+(1)=(2)", sothunhat, sothuhai, tong);
+        */
+    }
+}
